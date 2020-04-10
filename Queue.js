cfc828722b2facb832e7154b03e31ea7ad3d744e
Vincent Sanders <vincesanders@hotmail.com>
2020-04-10 11:13:19 -0500

    Built a JavaScript version of a Queue.

---
+++ Queue.js
@@ -0,0 +1,46 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue { //FIFO
+    constructor() {
+        this.first = null;
+    }
+    isEmpty() {
+        return this.first === null;
+    }
+    peek() { //return first value
+        if (this.isEmpty()) throw new Error('Queue is empty!');
+        return this.first.val;
+    }
+    //add to end of queue - returns boolean for success
+    add(value) {
+        if (this.isEmpty()) {
+            this.first = new Node(value);
+            return true;
+        }
+        let currntNode = this.first;
+        while (currntNode.next) {
+            currntNode = currntNode.next;
+        }
+        currntNode.next = new Node(value);
+        return true;
+    }
+    //remove first item and return it
+    remove() {
+        if (this.isEmpty()) throw new Error('Queue is empty!');
+        const value = this.first.value;
+        this.first = this.first.next;
+        return value;
+    }
+}
+
+const q = new Queue();
+q.add(5);
+q.add(10);
+q.add(345);
+q.add(243);
+console.log(q.first);
